79e4e8924178961147e30d2f964b2faae9f2c164
Fela Ameghino <frayx@live.it>
2021-02-21 23:25:46 +0100

    Superellipse

---
+++ Clubhouse/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Clubhouse.Navigation;
+using Clubhouse.Navigation.Services;
+using Clubhouse.Services;
+using Clubhouse.Views;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Clubhouse
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : BootStrapper
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            var navService = WindowContext.GetForCurrentView().NavigationServices.GetByFrameId($"0");
+            if (navService != null)
+            {
+                ClubhouseSession.load();
+
+                if (ClubhouseSession.isLoggedIn())
+                {
+                    navService.Navigate(typeof(MainPage));
+                }
+                else
+                {
+                    navService.Navigate(typeof(LoginPage));
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public override UIElement CreateRootElement(IActivatedEventArgs e)
+        {
+            var navigationFrame = new Frame();
+            var navigationService = NavigationServiceFactory(BackButton.Ignore, ExistingContent.Include, navigationFrame, 0, $"0", true) as NavigationService;
+
+            return navigationFrame;
+        }
+
+        public override UIElement CreateRootElement(INavigationService navigationService)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Clubhouse/Controls/ProfilePicture.cs
+++ Clubhouse/Controls/ProfilePicture.cs
@@ -1,10 +1,10 @@
-﻿using Clubhouse.Common;
-using Microsoft.Graphics.Canvas.Geometry;
+﻿using Microsoft.Graphics.Canvas.Geometry;
 using System;
+using Windows.Foundation;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
-using Windows.UI.Xaml.Media;
 
 namespace Clubhouse.Controls
 {
@@ -13,20 +13,44 @@ namespace Clubhouse.Controls
         public ProfilePicture()
         {
             DefaultStyleKey = typeof(ProfilePicture);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            UpdateClip(finalSize.Width, finalSize.Height);
+            return base.ArrangeOverride(finalSize);
+        }
+
+        private void UpdateClip(double width, double height, double eccentricity = 2.5)
+        {
+            double x = 0;
+            double y = 0;
+
+            double halfWidth = width / 2.0;
+            double halfHeight = height / 2.0;
+
+            double centreX = x + halfWidth;
+            double centreY = y + halfHeight;
+
+            double TWO_PI = Math.PI * 2.0;
+            int resolution = 100;
 
             var builder = new CanvasPathBuilder(null);
-            /* M18.947 18.947
-             * C5.627 32.267 1 61.517 1 128
-             * s4.626 95.732 17.947 109.053
-             * C32.267 250.373 61.517 255 128 255
-             * s95.732-4.626 109.053-17.947
-             * C250.373 223.733 255 194.483 255 128
-             * s-4.626-95.732-17.947-109.053
-             * C223.733 5.627 194.483 1 128 1
-             * S32.268 5.626 18.947 18.947z
-             */
+            builder.BeginFigure((float)(x + width), (float)centreY);
 
-            var path = CompositionPathParser.Parse("M3.4,3.4C0.9,5.9,0,11.4,0,24s0.9,18.1,3.4,20.6c2.5,2.5,8,3.4,20.6,3.4s18.1-0.9,20.6-3.4c2.5-2.5,3.4-8,3.4-20.6S47.1,5.9,44.6,3.4C42.1,0.9,36.6,0,24,0S5.9,0.9,3.4,3.4z");
+            for (double theta = 0.0; theta < TWO_PI; theta += TWO_PI / resolution)
+            {
+                double sineTheta = Math.Sin(theta);
+                double cosineTheta = Math.Cos(theta);
+                double r =
+                    Math.Pow(1 / (Math.Pow(Math.Abs(cosineTheta) / halfWidth, eccentricity) + Math.Pow(Math.Abs(sineTheta) / halfHeight, eccentricity)),
+                        1 / eccentricity);
+                builder.AddLine((float)(centreX + r * cosineTheta), (float)(centreY + r * sineTheta));
+            }
+
+            builder.EndFigure(CanvasFigureLoop.Closed);
+
+            var path = new CompositionPath(CanvasGeometry.CreatePath(builder));
             var visual = ElementCompositionPreview.GetElementVisual(this);
             visual.Clip = Window.Current.Compositor.CreateGeometricClip(Window.Current.Compositor.CreatePathGeometry(path));
         }
